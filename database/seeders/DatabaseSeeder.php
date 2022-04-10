f4c6e792d58d2ef1fb51a204bc14f37f986b6e27
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-04-11 00:34:28 +0600

    Dashboard Template include and Dashboard group route created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,24 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name'              => 'Abdulaha Islam',
+            'username'          => 'abdulaha1715',
+            'email'             => 'abdulahaislam210917@gmail.com',
+            'password'          => bcrypt('01918786189'),
+            'email_verified_at' => now(),
+            'avatar'            => 'https://picsum.photos/300',
+        ]);
+
+
+        User::create([
+            'name'              => 'Admin',
+            'username'          => 'admin',
+            'email'             => 'admin@gmail.com',
+            'password'          => bcrypt('admin'),
+            'email_verified_at' => now(),
+            'avatar'            => 'https://picsum.photos/300',
+        ]);
     }
 }
