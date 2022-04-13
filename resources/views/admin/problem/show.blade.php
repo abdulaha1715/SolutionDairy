8465c6d995dc632d2f4472ea8ea2a30656fc0a1a
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-04-14 01:25:05 +0600

    Problem answer accordion added

--- resources/views/admin/problem/show.blade.php
+++ resources/views/admin/problem/show.blade.php
@@ -127,31 +127,144 @@
         <!-- Sales Overview -->
         <div class="card mt-6">
             <!-- header -->
-            <div class="card-header flex flex-row justify-between">
-                <h1 class="h6">Problem Solution </h1>
-
-                <div class="flex flex-row justify-center items-center">
-                    <a href="">
-                        <i class="fad fa-chevron-double-down mr-6"></i>
-                    </a>
-
-                    <a href="">
-                        <i class="fad fa-ellipsis-v"></i>
-                    </a>
-                </div>
+            <div class="card-header flex flex-row justify-between accordion">
+                <h1 class="h6">Problem Solution # </h1>
             </div>
             <!-- end header -->
 
             <!-- body -->
-            <div class="card-body grid grid-cols-2 gap-6 lg:grid-cols-1">
-                <div class="p-0">
+            <div class="grid grid-cols-2 gap-6 lg:grid-cols-1 panel p-0">
+                <div class="p-6">
                     <div class="mb-10 items-center">
                         <h4 class="h4">Solution</h4>
                         <p class="text-black">Amore sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.% more sales in comparison to last month.</p>
                     </div>
                 </div>
 
-                <div class="">
+                <div class="pt-6 ">
+                    <div class="problem-screenshort ">
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- end body -->
+        </div>
+        <!-- end Sales Overview -->
+
+        <!-- Sales Overview -->
+        <div class="card">
+            <!-- header -->
+            <div class="card-header flex flex-row justify-between accordion">
+                <h1 class="h6">Problem Solution # </h1>
+            </div>
+            <!-- end header -->
+
+            <!-- body -->
+            <div class="grid grid-cols-2 gap-6 lg:grid-cols-1 panel p-0">
+                <div class="p-6">
+                    <div class="mb-10 items-center">
+                        <h4 class="h4">Solution</h4>
+                        <p class="text-black">Amore sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.% more sales in comparison to last month.</p>
+                    </div>
+                </div>
+
+                <div class="pt-6 ">
+                    <div class="problem-screenshort ">
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- end body -->
+        </div>
+        <!-- end Sales Overview -->
+
+        <!-- Sales Overview -->
+        <div class="card">
+            <!-- header -->
+            <div class="card-header flex flex-row justify-between accordion">
+                <h1 class="h6">Problem Solution # </h1>
+            </div>
+            <!-- end header -->
+
+            <!-- body -->
+            <div class="grid grid-cols-2 gap-6 lg:grid-cols-1 panel p-0">
+                <div class="p-6">
+                    <div class="mb-10 items-center">
+                        <h4 class="h4">Solution</h4>
+                        <p class="text-black">Amore sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.% more sales in comparison to last month.</p>
+                    </div>
+                </div>
+
+                <div class="pt-6 ">
+                    <div class="problem-screenshort ">
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                        <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="https://picsum.photos/800/800" class="m-1" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- end body -->
+        </div>
+        <!-- end Sales Overview -->
+
+        <!-- Sales Overview -->
+        <div class="card">
+            <!-- header -->
+            <div class="card-header flex flex-row justify-between accordion">
+                <h1 class="h6">Problem Solution # </h1>
+            </div>
+            <!-- end header -->
+
+            <!-- body -->
+            <div class="grid grid-cols-2 gap-6 lg:grid-cols-1 panel p-0">
+                <div class="p-6">
+                    <div class="mb-10 items-center">
+                        <h4 class="h4">Solution</h4>
+                        <p class="text-black">Amore sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.more sales in comparison to last month.% more sales in comparison to last month.</p>
+                    </div>
+                </div>
+
+                <div class="pt-6 ">
                     <div class="problem-screenshort ">
                         <a href="https://picsum.photos/800/800" data-fancybox="gallery" data-caption="Optional caption">
                             <img src="https://picsum.photos/800/800" class="m-1" alt="">
