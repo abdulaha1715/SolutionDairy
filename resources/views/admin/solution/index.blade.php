b37a9bd1d879f635e1f75f616f512dc10af84929
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-04-27 18:01:38 +0600

    Solution index edit

--- resources/views/admin/solution/index.blade.php
+++ resources/views/admin/solution/index.blade.php
@@ -34,7 +34,6 @@
                             <td class="border border-l-0 border-r-0 px-4 py-2 w-40">
                                 <div class="flex justify-center">
                                     <a href="{{ route('solution.edit', $solution) }}" class="btn-bs-primary mx-1">Edit</a>
-                                    <a href="{{ route('solution.show', $solution) }}" class="btn-bs-success mx-1">View</a>
                                     <form action="{{ route('solution.destroy', $solution) }}" method="POST" onsubmit="return confirm('Do you want to delete?');">
                                         @csrf
                                         @method('DELETE')
